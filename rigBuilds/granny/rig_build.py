88de0b1e036a408041e3fa2dc1e34408de00b630
Daniel Ruiz <d.ruiz@rubika.ca.local>
2023-11-09 20:58:17 -0500

    Adding some missing functions

--- rigBuilds/granny/rig_build.py
+++ rigBuilds/granny/rig_build.py
@@ -11,7 +11,10 @@ def build():
     import_geometry()
     import_guides()
     build_rig()
-
+    custom_rig()
+    load_skinning_data()
+    load_shapes_data()
+    cleanup()
 
 
 def import_geometry():
@@ -25,6 +28,16 @@ def build_rig():
     io.build_template()
 
 
+def custom_rig():
+    pass
+def load_skinning_data():
+    pass
+
+def load_shapes_data():
+    pass
+
+def cleanup():
+    pass
 
 if __name__ == '__main__':
     build()
