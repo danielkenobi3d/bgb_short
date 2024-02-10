ddb7d30380313b661717670a94de7ca6120ae87f
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-02-09 19:32:09 -0500

    test in environment

--- pipeline/pipe_config.py
+++ pipeline/pipe_config.py
@@ -1,15 +1,14 @@
 import os
 
-asset_list = ['Grandma', 'Ghost', 'Guardian', 'Rick', 'grapplinHook']
-asset_types = ['characters', 'prop']
-project_path = 'Z:/07_CLASSE_ANIMATION_M1/Rigging/rig_build/assets'
-# 'Z:/06-CLASSE ANIMATION M2/Projects/bgb24/04-ASSETS/Characters'
-asset_path = '/{}'
-model_path = '/{}_Mod'
-rig_path = '/{}_Rig'
-data_path = f'/{os.getlogin()}/data'
-publish_folder = '/publish'
-modules_path = 'bgb_short.rigBuilds'
+asset_list = ['GPa', 'BikeGpa']
+project_path = 'Z:/07_CLASSE_ANIMATION_M1/Ramses/Projects/laCalesita/04-ASSETS'
+
+asset_path = 'lacalesita_A_{}'
+model_path = 'lacalesita_A_{}_Mod'
+rig_path = 'lacalesita_A_{}_Rig'
+data_path = f'{os.getlogin()}/data'
+publish_folder = '_published'
+modules_path = 'LaCalesita.rigBuilds'
 default_module = 'default_character'
 default_facial_definition = 'facial_definition'
 default_geo_definition = 'geo_definition'
